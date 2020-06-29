16054dca611e97d760bca866e5f34aae372d9862
thejustjust <thejustjust@gmail.com>
2020-06-28 20:40:45 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def jobs():
+    @app.route('/')
+    @app.route('/jobs')
     return render_template('index.html')
-
-@app.route('/')
-@app.route('/jobs')
+    
