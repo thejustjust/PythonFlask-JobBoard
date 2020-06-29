f6ad13ee3fb77efbb87f5be79f09837510ed2853
thejustjust <thejustjust@gmail.com>
2020-06-28 21:53:54 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -2,7 +2,14 @@ import sqlite3
 from flask import g, Flask, render_template
 
 app = Flask(__name__)
+
 DATABASE = 'db/jobs.sqlite'
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
 
 @app.route("/")
 @app.route("/jobs")
